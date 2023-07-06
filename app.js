1141f640f3b3ee85b00c424e8ed7ae8743d13b46
truong.nn <truong.nn@amanotes.com>
2023-07-06 13:10:34 +0700

    create middlewares

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('In comes a ' + req.method + " to "  + req.url);
+  console.log("In comes a " + req.method + " to " + req.url);
   next();
-})
+});
+
+app.use((req, res, next) => {
+  const minutes = new Date().getMinutes();
+  if (minutes % 2 === 0) {
+    next();
+  } else {
+    res.statusCode = 403;
+    res.end("Not authorized");
+  }
+});
 
 app.use((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!\n');
-})
+  res.end('Secret info: the password is "swordfish"!');
+});
 
 // run
-http.createServer(app).listen(3000).on('listening', () => {
-  console.log('Listening on localhost:3000');
-})
+http
+  .createServer(app)
+  .listen(3000)
+  .on("listening", () => {
+    console.log("Listening on localhost:3000");
+  });
