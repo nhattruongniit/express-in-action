c32e54ea9d864cae7526c46defca6416187e77f4
truong.nn <truong.nn@amanotes.com>
2023-07-11 09:21:33 +0700

    test

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const logger = require("morgan");
 const http = require("http");
+const path = require("path");
+
 const app = express();
 
-app.use((req, res, next) => {
+// logger middlewares
+app.use(logger("short"));
+
+// static files
+const publicPath = path.resolve(__dirname, "public"); // setup the public path
+app.use(express.static(publicPath)); // send static files from public path
+
+app.use((req, _, next) => {
   console.log("In comes a " + req.method + " to " + req.url);
   next();
 });
 
-app.use((req, res, next) => {
+app.use((_, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Looks like you didn't find a static file.");
+  // next();
+});
+
+app.use((_, res, next) => {
   const minutes = new Date().getMinutes();
   if (minutes % 2 === 0) {
     next();
